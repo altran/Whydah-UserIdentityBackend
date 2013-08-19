aa657ca6ff9553aea52adb263d6298cc896f8716
Sherriff <erik-dev@fjas.no>
2013-08-19 22:21:42 +0200

    Configure email sending via gmail account.

--- src/main/java/net/whydah/iam/service/mail/MailSender.java
+++ src/main/java/net/whydah/iam/service/mail/MailSender.java
@@ -1,12 +1,10 @@
 package net.whydah.iam.service.mail;
 
+import net.whydah.iam.service.config.AppConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
@@ -16,44 +14,50 @@ import java.util.Properties;
  */
 public class MailSender {
     private static final Logger log = LoggerFactory.getLogger(MailSender.class);
+    public static final String FROM_ADDRESS = "notworking@whydah.net";
+    private final String username;
+    private final String password;
+
+    public MailSender() {
+        this.username = AppConfig.appConfig.getProperty("gmail.username");
+        this.password = AppConfig.appConfig.getProperty("gmail.password");
+    }
 
     /*
     public static void main(String[] args) {
-        new MailSender().send("erik@cantara.no", "Whydah MailSender test", "Dear Mail Crawler,"
-                + "\n\n No spam to my email, please!");
+        new MailSender().send("erik@cantara.no", "Whydah MailSender test", "Email via Gmail test");
     }
     */
 
     public void send(String recipients, String subject, String body) {
         log.debug("Sending email to recipients={}, subject={}, body={}", new String[]{recipients, subject, body});
-        final String username = "someEmail@gmail.com";
-        final String password = "";
 
-        /* //Gmail props
+        //Gmail props
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
-        */
 
         //Cantara smtp, will only work with @cantara-adresses
+        /*
         Properties props = new Properties();
         props.put("mail.smtp.auth", "false");
         props.put("mail.smtp.starttls.enable", "false");
         props.put("mail.smtp.host", "www.cantara.no");
         props.put("mail.smtp.port", "25");
-
-        Session session = Session.getInstance(props /*,
+        Session session = Session.getInstance(props);
+        */
+        Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
                         return new PasswordAuthentication(username, password);
                     }
-                } */);
+                });
 
         try {
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("notworking@whydah.net"));
+            message.setFrom(new InternetAddress(FROM_ADDRESS));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipients));
             message.setSubject(subject);
             message.setText(body);
