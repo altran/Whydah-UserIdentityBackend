ef1f5dfae37873e5f6d4f2e7f0eea221a748336f
Totto <totto@totto.org>
2013-09-23 20:56:10 +0200

    minor script fix

--- start-service.sh
+++ start-service.sh
@@ -9,4 +9,7 @@ JARFILE=$A-$V.jar
 pkill -f $A
 
 wget --user=altran --password=l1nkSys -O $JARFILE "http://mvnrepo.cantara.no/service/local/artifact/maven/content?r=altran-snapshots&g=net.whydah.sso.service&a=$A&v=$V&p=jar"
-java -jar -DIAM_CONFIG=useridentitybackend.TEST.properties $JARFILE &
+nohup java -jar -DIAM_CONFIG=useridentitybackend.TEST.properties $JARFILE &
+
+tail -f nohup.out
+
