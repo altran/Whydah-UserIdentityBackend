b1caf44b92e18883ec9580684e2041aa41df8487
Totto <totto@totto.org>
2013-12-19 09:56:46 +0100

    oops :)

--- src/test/java/net/whydah/iam/service/mail/EmailBodyGeneratorTest.java
+++ src/test/java/net/whydah/iam/service/mail/EmailBodyGeneratorTest.java
@@ -23,7 +23,7 @@ public class EmailBodyGeneratorTest {
     @Test
     public void resetPassword() {
         String url = "https://sso.whydah.net/newuser/jkhg4jkhg4kjhg4kjhgk3jhg4kj3grkj34hgr3hk4gk3rjhg4kj3hgr4kj3";
-        String newUserEmailBody = emailBodyGenerator.resetPassword(url);
+        String newUserEmailBody = emailBodyGenerator.resetPassword(url,"username");
         assertTrue(newUserEmailBody.contains(url));
     }
 }
