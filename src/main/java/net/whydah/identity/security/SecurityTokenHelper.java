6b2c42766d0d19db42c01cc28fceedc252d7a8fd
davlet <davlet.d@gmail.com>
2014-03-29 15:09:19 +0100

    rename "iam" to "token" in tokenservice path. related to changes in tokenservice

--- src/main/java/net/whydah/identity/security/SecurityTokenHelper.java
+++ src/main/java/net/whydah/identity/security/SecurityTokenHelper.java
@@ -42,8 +42,8 @@ public class SecurityTokenHelper {
         MultivaluedMap<String,String> formData = new MultivaluedMapImpl();
         formData.add("usertokenid", usertokenid);
         formData.add("apptoken", myAppTokenXML);
-        ClientResponse response = wr.path("iam/" + appTokenId + "/getusertokenbytokenid").type(MediaType.APPLICATION_FORM_URLENCODED_TYPE).post(ClientResponse.class, formData);
-        logger.info("Accessing:"+"iam/" + appTokenId + "/getusertokenbytokenid");
+        ClientResponse response = wr.path("token/" + appTokenId + "/getusertokenbytokenid").type(MediaType.APPLICATION_FORM_URLENCODED_TYPE).post(ClientResponse.class, formData);
+        logger.info("Accessing:"+"token/" + appTokenId + "/getusertokenbytokenid");
         if(response.getStatus() == Response.Status.OK.getStatusCode()) {
             String usertoken = response.getEntity(String.class);
             logger.debug("usertoken: {}", usertoken);
