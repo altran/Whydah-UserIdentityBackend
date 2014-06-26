8e9f5a76204b1b98cac640d65e884ddfeed8f391
Stig Lau <stig@lau.no>
2014-06-26 12:09:00 +0200

    Fixed null-test

--- src/main/java/net/whydah/identity/security/SecurityFilter.java
+++ src/main/java/net/whydah/identity/security/SecurityFilter.java
@@ -143,7 +143,7 @@ public class SecurityFilter implements Filter {
      * @return authentication
      */
     protected String findUserTokenId(String pathInfo) {
-        if(!pathInfo.startsWith("/")) {
+        if(pathInfo != null && !pathInfo.startsWith("/")) {
             logger.error("Call to UIB does not start with '/' which can be because of configuration problem. Problematic Path: {}", pathInfo);
         }
         String tokenIdPath = findPathElement(pathInfo, 2);
