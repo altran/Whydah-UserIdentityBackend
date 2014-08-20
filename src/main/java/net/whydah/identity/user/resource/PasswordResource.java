6f9181a147304be9d787d09d17d3fbed428f8bed
Totto <totto@totto.org>
2014-08-20 18:31:04 +0200

    start work on set password from reset password

--- src/main/java/net/whydah/identity/user/resource/PasswordResource.java
+++ src/main/java/net/whydah/identity/user/resource/PasswordResource.java
@@ -48,4 +48,24 @@ public class PasswordResource {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+
+    @GET
+    @Path("/reset/username/{username}/newpassword/{token}")
+    public Response setPassword(@PathParam("username") String username,@PathParam("token") String token) {
+        log.info("newpassword for user {} token {}", username,token);
+        try {
+            UserIdentity user = userIdentityService.getUserIndentity(username);
+            if (user == null) {
+                return Response.status(Response.Status.NOT_FOUND).entity("User not found").build();
+            }
+            // decode password from token
+            // set new password
+
+            return Response.ok().build();
+        } catch (Exception e) {
+            log.error("newpassword failed", e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
