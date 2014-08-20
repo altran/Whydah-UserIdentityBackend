2ac0f96034d3acbc8a60f14ba6895e8a76fa8228
baardl <bard.lind@gmail.com>
2014-08-20 22:09:18 +0200

    #28 Reset password with token and passwordjson.

--- src/main/java/net/whydah/identity/user/resource/PasswordResource.java
+++ src/main/java/net/whydah/identity/user/resource/PasswordResource.java
@@ -3,10 +3,13 @@ package net.whydah.identity.user.resource;
 import com.google.inject.Inject;
 import net.whydah.identity.user.identity.UserIdentity;
 import net.whydah.identity.user.identity.UserIdentityService;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
@@ -50,17 +53,35 @@ public class PasswordResource {
     }
 
 
-    @GET
+    @POST
     @Path("/reset/username/{username}/newpassword/{token}")
-    public Response setPassword(@PathParam("username") String username,@PathParam("token") String token) {
+    public Response setPassword(@PathParam("username") String username,@PathParam("token") String token,String passwordJson) {
         log.info("newpassword for user {} token {}", username,token);
         try {
             UserIdentity user = userIdentityService.getUserIndentity(username);
             if (user == null) {
                 return Response.status(Response.Status.NOT_FOUND).entity("User not found").build();
             }
-            // decode password from token
-            // set new password
+            boolean ok;
+            try {
+                ok = userIdentityService.authenticateWithChangePasswordToken(username, token);
+            } catch (RuntimeException re) {
+                log.error("changePasswordForUser-RuntimeException username {}, message {}", username,re.getMessage(), re);
+                return Response.status(Response.Status.BAD_REQUEST).build();
+            }
+
+            if (!ok) {
+                log.info("Authentication failed while changing password for user {}", username);
+                return Response.status(Response.Status.FORBIDDEN).build();
+            }
+            try {
+                JSONObject jsonobj = new JSONObject(passwordJson);
+                String newpassword = jsonobj.getString("newpassword");
+                userIdentityService.changePassword(username, user.getUid(), newpassword);
+            } catch (JSONException e) {
+                log.error("Bad json", e);
+                return Response.status(Response.Status.BAD_REQUEST).build();
+            }
 
             return Response.ok().build();
         } catch (Exception e) {
