c7f3cfc98ba5dd51bfbcddc3c2ccce9746af382d
Bard Lind <bard.lind@tomra.com>
2014-09-04 11:17:51 +0200

    #36 Adding user(s) found in LDAP to Lucene Index

--- src/main/java/net/whydah/identity/user/search/UserSearch.java
+++ src/main/java/net/whydah/identity/user/search/UserSearch.java
@@ -17,11 +17,13 @@ public class UserSearch {
     private static final Logger log = LoggerFactory.getLogger(UserSearch.class);
     private final LdapUserIdentityDao ldapUserIdentityDao;
     private final LuceneSearch luceneSearch;
+    private final LuceneIndexer luceneIndexer;
 
     @Inject
-    public UserSearch(LdapUserIdentityDao ldapUserIdentityDao, LuceneSearch luceneSearch) {
+    public UserSearch(LdapUserIdentityDao ldapUserIdentityDao, LuceneSearch luceneSearch, LuceneIndexer luceneIndexer) {
         this.ldapUserIdentityDao = ldapUserIdentityDao;
         this.luceneSearch = luceneSearch;
+        this.luceneIndexer = luceneIndexer;
     }
 
     public List<UserIdentityRepresentation> search(String query) {
@@ -32,6 +34,9 @@ public class UserSearch {
                 UserIdentity user = ldapUserIdentityDao.getUserIndentity(query);
                 if (user != null) {
                     users.add(user);
+                    //Update user to lucene.
+                    log.trace("Adding a new user foound in LDAP to lucene index. User {}", user.toString());
+                    luceneIndexer.update(user);
                 }
             } catch (NamingException e) {
                 log.warn("Could not find users from ldap/AD. Query: {}", query, e);
