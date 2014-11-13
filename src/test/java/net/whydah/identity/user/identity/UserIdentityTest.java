6a5e07d4a431546332c35704af1aba284e3c79c6
Sherriff <erik-dev@fjas.no>
2014-11-13 10:08:22 +0100

    Fix personRef test.

--- src/test/java/net/whydah/identity/user/identity/UserIdentityTest.java
+++ src/test/java/net/whydah/identity/user/identity/UserIdentityTest.java
@@ -2,8 +2,6 @@ package net.whydah.identity.user.identity;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
-
 /**
  * @author <a href="mailto:erik-dev@fjas.no">Erik Drolshammer</a> 2014-11-05
  */
@@ -30,25 +28,10 @@ public class UserIdentityTest {
     @Test
     public void testValidatePersonRefOK() {
         UserIdentity userIdentity = new UserIdentity("uid1", "username1", "firstName1", "lastName1", "valid@email.dk", "password1", null, "personRef1");
-        String[] personRefs = new String[]{"0", "123", "abc", "123abc", "", null};
+        String[] personRefs = new String[]{"0", "123", "abc", "123abc", "valid@email.dk", "123-456", "123/456", "", null};
         for (String personRef : personRefs) {
             userIdentity.setPersonRef(personRef);
             userIdentity.validate();
         }
     }
-    @Test
-    public void testValidatePersonRefThrowsException() {
-        UserIdentity userIdentity = new UserIdentity("uid1", "username1", "firstName1", "lastName1", "valid@email.dk", "password1", null, "personRef1");
-        String[] personRefs = new String[]{"valid@email.dk", "123-456", "123/456"};
-        for (String personRef : personRefs) {
-            userIdentity.setPersonRef(personRef);
-            try {
-                userIdentity.validate();
-                fail("Expected InvalidUserIdentityFieldException.");
-            } catch (InvalidUserIdentityFieldException e) {
-                //Expect exception
-            }
-        }
-    }
-
 }
