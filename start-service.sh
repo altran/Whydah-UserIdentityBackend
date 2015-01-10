a120356ce5c01ce15fab7d86a4c716be020218c2
Totto <totto@totto.org>
2015-01-10 20:39:26 +0100

    synced startup and update scripts

--- start-service.sh
+++ start-service.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup /usr/bin/java -DIAM_MODE=PROD -DIAM_CONFIG=/home/UserIdentityBackend/useridentitybackend.PROD.properties -jar /home/UserIdentityBackend/UserIdentityBackend.jar
+nohup /usr/bin/java -DIAM_MODE=PROD -DIAM_CONFIG=/home/UserIdentityBackend/useridentitybackend.PROD.properties -jar /home/UserIdentityBackend/UserIdentityBackend.jar &
