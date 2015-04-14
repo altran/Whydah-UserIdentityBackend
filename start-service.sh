97ce5820fba37a0a53c268c0c8d07c10150537c0
Thor Henning Hetland <totto@totto.org>
2015-04-14 16:19:03 +0200

    parametrized

--- start-service.sh
+++ start-service.sh
@@ -1,2 +1,24 @@
 #!/bin/sh
-nohup /usr/bin/java -DIAM_MODE=PROD -DIAM_CONFIG=/home/UserIdentityBackend/useridentitybackend.PROD.properties -jar /home/UserIdentityBackend/UserIdentityBackend.jar &
+
+#  If IAM_MODE not set, use PROD
+if [ -z "$IAM_MODE" ]
+  IAM_MODE=PROD
+fi
+
+# If IAM_CONFOG not set, use PROD properties from local directory
+if [ -z "$IAM_CONFIG" ]
+  IAM_CONFIG=useridentitybackend.PROD.properties
+fi
+
+# If Version is from source, find the artifact
+if [ $Version = "FROM_SOURCE" ]; then 
+    # Find the bult artifact
+    Version=$(find target/* -name *.jar | grep SNAPSHOT | grep -v original | gre
+p -v lib)
+else
+    Version=UserIdentityBackend.jar
+fi
+
+
+nohup /usr/bin/java -DIAM_MODE=$IAM_MODE -DIAM_CONFIG=$IAM_CONFIG -jar  $Version
+
