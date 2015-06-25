4dbff29c1455fb8a435adfdd8f1ff5228d88345d
Sherriff <erik-dev@fjas.no>
2015-06-25 15:15:02 +0200

    renamed RESTApplication to JerseyApplication.

--- src/main/java/net/whydah/identity/config/JerseyApplication.java
+++ src/main/java/net/whydah/identity/config/JerseyApplication.java
@@ -10,22 +10,12 @@ import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.ApplicationPath;
 
-@ApplicationPath("dc")
-public class RESTApplication extends ResourceConfig {
-    private static final Logger log = LoggerFactory.getLogger(RESTApplication.class);
+@ApplicationPath("overriddenByWebXml")
+public class JerseyApplication extends ResourceConfig {
+    private static final Logger log = LoggerFactory.getLogger(JerseyApplication.class);
 
-    public RESTApplication() {
+    public JerseyApplication() {
         //https://java.net/jira/browse/JERSEY-2175
-        /*
-        net.whydah.identity.application.ApplicationsResource
-        net.whydah.identity.application.ApplicationResource
-        net.whydah.identity.user.resource.PasswordResource
-        net.whydah.identity.user.authentication.UserAuthenticationEndpoint
-        net.whydah.identity.health.HealthResource
-        net.whydah.identity.user.resource.UsersResource
-        net.whydah.identity.user.resource.UserResource
-         */
-        //ResourceConfig resourceConfig = packages("net.whydah.identity.application", "net.whydah.identity.user", "net.whydah.identity.health");
         //Looks like recursive scanning is not working when specifying multiple packages.
         ResourceConfig resourceConfig = packages("net.whydah.identity");
         resourceConfig.register(org.glassfish.jersey.server.mvc.freemarker.FreemarkerMvcFeature.class);
