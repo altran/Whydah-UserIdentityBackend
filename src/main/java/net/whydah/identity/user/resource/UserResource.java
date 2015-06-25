af45d96efa1a29069e7923c82672344e714b09df
Sherriff <erik-dev@fjas.no>
2015-06-25 17:11:08 +0200

    delete unused ApplicationDao reference in UserResource.

--- src/main/java/net/whydah/identity/user/resource/UserResource.java
+++ src/main/java/net/whydah/identity/user/resource/UserResource.java
@@ -34,8 +34,6 @@ public class UserResource {
 
     private final UserIdentityService userIdentityService;
     private final UserAggregateService userAggregateService;
-    private final ApplicationDao applicationDao;
-
     private final ObjectMapper mapper;
 
     @Context
@@ -45,7 +43,6 @@ public class UserResource {
     public UserResource(UserIdentityService userIdentityService, UserAggregateService userAggregateService, ApplicationDao applicationDao) {
         this.userIdentityService = userIdentityService;
         this.userAggregateService = userAggregateService;
-        this.applicationDao = applicationDao;
         this.mapper = new ObjectMapper();
         log.info("Started: UserResource");
     }
@@ -315,6 +312,4 @@ public class UserResource {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }
-
-
 }
