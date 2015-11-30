5743d59964eae2b4f21e41fb228fc70bed22f1d5
Thor Henning Hetland <totto@totto.org>
2015-11-30 22:21:05 +0100

    added lucene applications in application import

--- src/main/java/net/whydah/identity/dataimport/ApplicationJsonImporter.java
+++ src/main/java/net/whydah/identity/dataimport/ApplicationJsonImporter.java
@@ -1,16 +1,15 @@
 package net.whydah.identity.dataimport;
 
 import net.whydah.identity.application.ApplicationService;
+import net.whydah.identity.application.search.LuceneApplicationIndexer;
 import net.whydah.sso.application.mappers.ApplicationMapper;
 import net.whydah.sso.application.types.Application;
+import org.apache.lucene.store.NIOFSDirectory;
+import org.constretto.ConstrettoConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.*;
 import java.util.List;
 
 
@@ -20,10 +19,17 @@ public class ApplicationJsonImporter {
 
 
     private ApplicationService applicationService;
+    private final String luceneApplicationDir;
+    private final LuceneApplicationIndexer luceneApplicationIndexer;
 
 
-    public ApplicationJsonImporter(ApplicationService applicationService) {
+
+    public ApplicationJsonImporter(ApplicationService applicationService, ConstrettoConfiguration configuration) {
         this.applicationService = applicationService;
+        this.luceneApplicationDir = configuration.evaluateToString("lucene.directory");
+        NIOFSDirectory index = createDirectory(luceneApplicationDir);
+        luceneApplicationIndexer = new LuceneApplicationIndexer(index);
+
     }
 
     public void importApplications(InputStream applicationsSource) {
@@ -63,11 +69,26 @@ public class ApplicationJsonImporter {
         for (Application application: applications) {
             try {
                 applicationService.create(application.getId(), application);
+                luceneApplicationIndexer.addToIndex(application);
                 log.info("Imported Application. Id {}, Name {}", application.getId(), application.getName());
             } catch(Exception e) {
                 log.error("Unable to persist application: {}", application.toString(), e);
                 throw new RuntimeException("Unable to persist application: " + application.toString(), e);
             }
+        }
+    }
+    private NIOFSDirectory createDirectory(String luceneDir) {
+        try {
+            File luceneDirectory = new File(luceneDir);
+            if (!luceneDirectory.exists()) {
+                boolean dirsCreated = luceneDirectory.mkdirs();
+                if (!dirsCreated) {
+                    log.debug("{} was not successfully created.", luceneDirectory.getAbsolutePath());
+                }
+            }
+            return new NIOFSDirectory(luceneDirectory);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
