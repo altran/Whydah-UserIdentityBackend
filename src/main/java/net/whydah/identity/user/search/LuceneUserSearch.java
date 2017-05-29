6f2c400ada0f99667cab3a98faea8006dfb8d020
totto <totto@totto.org>
2017-05-29 15:43:01 +0200

    extended the user search to return max 2500 users (from 500)

--- src/main/java/net/whydah/identity/user/search/LuceneUserSearch.java
+++ src/main/java/net/whydah/identity/user/search/LuceneUserSearch.java
@@ -26,7 +26,7 @@ import java.util.List;
 public class LuceneUserSearch {
     private static final Logger logger = LoggerFactory.getLogger(LuceneUserSearch.class);
     protected static final Analyzer ANALYZER = new StandardAnalyzer();  //use LuceneUserIndexer.ANALYZER?
-    private static final int MAX_HITS = 2500;
+    private static final int MAX_HITS = 2600;
     private final Directory index;
 
 
