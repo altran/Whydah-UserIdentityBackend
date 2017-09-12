e4827bba6c375aa359c4f9a5a52942439617f753
totto <totto@totto.org>
2017-09-12 10:07:42 +0200

    more simulated data in applicationsearchtest

--- src/test/java/net/whydah/identity/application/search/LuceneApplicationSearchTest.java
+++ src/test/java/net/whydah/identity/application/search/LuceneApplicationSearchTest.java
@@ -9,8 +9,10 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class LuceneApplicationSearchTest {
 
@@ -22,17 +24,17 @@ public class LuceneApplicationSearchTest {
         RAMDirectory index = new RAMDirectory();
         addApplications(index);
 
-        LuceneApplicationSearch luceneSearch = new LuceneApplicationSearch(index);
-        List<Application> result = luceneSearch.search("Whydah");
-        assertEquals(9, result.size());
-        List<Application> result2 = luceneSearch.search("SecurityTokenService");
+        LuceneApplicationSearch luceneApplicationSearch = new LuceneApplicationSearch(index);
+        List<Application> result = luceneApplicationSearch.search("Whydah");
+        assertTrue(result.size() > 8);
+        List<Application> result2 = luceneApplicationSearch.search("SecurityTokenService");
         assertEquals(1, result2.size());
-        List<Application> result3 = luceneSearch.search("ACS");
+        List<Application> result3 = luceneApplicationSearch.search("ACS");
         assertEquals(2, result3.size());
-        List<Application> result4 = luceneSearch.search("whydahdev");
+        List<Application> result4 = luceneApplicationSearch.search("whydahdev");
         assertEquals(6, result4.size());
-        List<Application> result5 = luceneSearch.search("*");
-        assertEquals(9, result5.size());
+        List<Application> result5 = luceneApplicationSearch.search("*");
+        assertTrue(result5.size() >= LuceneApplicationSearch.MAX_HITS);
 
     }
 
@@ -42,12 +44,12 @@ public class LuceneApplicationSearchTest {
         Directory index = new RAMDirectory();
         addApplications(index);
 
-        LuceneApplicationSearch luceneSearch = new LuceneApplicationSearch(index);
-        List<Application> result = luceneSearch.search("Whydah");
-        assertEquals(9, result.size());
-        result = luceneSearch.search("ACS");
+        LuceneApplicationSearch luceneApplicationSearch = new LuceneApplicationSearch(index);
+        List<Application> result = luceneApplicationSearch.search("Whydah");
+        assertTrue(result.size() > 8);
+        result = luceneApplicationSearch.search("ACS");
         assertEquals(2, result.size());
-        result = luceneSearch.search("SecurityTokenService");
+        result = luceneApplicationSearch.search("SecurityTokenService");
         assertEquals(1, result.size());
 
     }
@@ -55,6 +57,13 @@ public class LuceneApplicationSearchTest {
     private LuceneApplicationIndexer addApplications(Directory index) throws IOException {
         LuceneApplicationIndexer luceneIndexer = new LuceneApplicationIndexer(index);
         List<Application> applications = ApplicationMapper.fromJsonList(ApplicationHelper.getDummyAppllicationListJson());
+        Application extensionApplication = applications.get(8);
+        for (int n = 0; n < 10000; n++) {
+            Application newApp = ApplicationMapper.fromJson(ApplicationMapper.toJson(extensionApplication));
+            newApp.setName("Simulated application " + n);
+            newApp.setId(UUID.randomUUID().toString());
+            applications.add(newApp);
+        }
         luceneIndexer.addToIndex(applications);
         return luceneIndexer;
     }
