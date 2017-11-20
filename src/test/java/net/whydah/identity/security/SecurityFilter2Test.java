0421b9e583e77d984093a39fad658e6d7f5e30cd
totto <totto@totto.org>
2017-11-20 14:55:34 +0100

    fixed testdata

--- src/test/java/net/whydah/identity/security/SecurityFilter2Test.java
+++ src/test/java/net/whydah/identity/security/SecurityFilter2Test.java
@@ -11,6 +11,7 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertEquals;
@@ -26,9 +27,39 @@ public class SecurityFilter2Test {
     private SecurityFilter securityFilter;
     private HealthCheckService healthCheckService;
 
-    private final static String userAdminUserTokenId = "au123";
-    private final static String tokenBrukeradmin = "<application ID=\"1\"><organizationName>2</organizationName><role name=\"WhydahUserAdmin\"/></application>";
+    private final static String userAdminUserTokenId = UUID.randomUUID().toString();
+    private final static String tokenBrukeradmin = "<application ID=\"1322\"><organizationName>2323</organizationName><role name=\"WhydahUserAdmin\"/></application>";
 
+    String userTokenXML = "<usertoken xmlns:ns2=\"http://www.w3.org/1999/xhtml\" id=\"a96a517f-cef3-4be7-92f5-f059b65e4071\">\n" +
+            "    <uid>myuid</uid>\n" +
+            "    <timestamp></timestamp>\n" +
+            "    <lifespan>3600000</lifespan>\n" +
+            "    <issuer>/token/issuer/tokenverifier</issuer>\n" +
+            "    <securitylevel>0</securitylevel>\n" +
+            "    <username>test</username>\n" +
+            "    <cellphone>90088900</cellphone>\n" +
+            "    <firstname>Olav</firstname>\n" +
+            "    <lastname>Nordmann</lastname>\n" +
+            "    <email></email>\n" +
+            "    <personRef></personRef>\n" +
+            "    <lastSeen></lastSeen>  <!-- Whydah 2.1 date and time of last registered user session -->\n" +
+            "    <application ID=\"2349785543\">\n" +
+            "        <applicationName>Whydah.net</applicationName>\n" +
+            "           <organizationName>Kunde 3</organizationName>\n" +
+            "              <role name=\"WhydahUserAdmin\" value=\"\"/>\n" +
+            "              <role name=\"president\" value=\"\"/>\n" +
+            "           <organizationName>Kunde 4</organizationName>\n" +
+            "              <role name=\"styremedlem\" value=\"\"/>\n" +
+            "    </application>\n" +
+            "    <application ID=\"appa\">\n" +
+            "        <applicationName>whydag.org</applicationName>\n" +
+            "        <organizationName>Kunde 1</organizationName>\n" +
+            "        <role name=\"styremedlem\" value=\"Valla\"/>\n" +
+            "    </application>\n" +
+            " \n" +
+            "    <ns2:link type=\"application/xml\" href=\"/\" rel=\"self\"/>\n" +
+            "    <hash type=\"MD5\">8a37ef9624ed93db4873035b0de3d1ca</hash>\n" +
+            "</usertoken>";
 
     @Before
     public void setup() throws ServletException {
@@ -74,8 +105,8 @@ public class SecurityFilter2Test {
 
     @Test
     public void testUsertokenIdAuthenticationOK() {
-        String appTokenId = "appTokenIdUser";
-        when(stsHelper.getUserToken(userAdminUserTokenId)).thenReturn(UserTokenMapper.fromUserTokenXml(tokenBrukeradmin));
+        String appTokenId = UUID.randomUUID().toString();
+        when(stsHelper.getUserToken(userAdminUserTokenId)).thenReturn(UserTokenMapper.fromUserTokenXml(userTokenXML));
         assertNull(securityFilter.authenticateAndAuthorizeRequest("/" + appTokenId + "/" + userAdminUserTokenId + "/user"));
         assertEquals(Authentication.getAuthenticatedUser().getRoleList().get(0).getRoleName(), "WhydahUserAdmin");
         assertNull(securityFilter.authenticateAndAuthorizeRequest("/" + appTokenId + "/" + userAdminUserTokenId + "/application"));
