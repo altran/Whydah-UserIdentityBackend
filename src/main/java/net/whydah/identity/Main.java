4750d6d94cf3700a491186ddfe263f284429d6a1
totto <totto@totto.org>
2017-12-11 08:10:20 +0100

    attempt to find out why UIB won't start correctly

--- src/main/java/net/whydah/identity/Main.java
+++ src/main/java/net/whydah/identity/Main.java
@@ -97,7 +97,7 @@ public class Main {
             Runtime.getRuntime().addShutdownHook(new Thread() {
                 public void run() {
                     log.debug("ShutdownHook triggered. Exiting application");
-                    main.stop();
+                    //main.stop();
                 }
             });
 
